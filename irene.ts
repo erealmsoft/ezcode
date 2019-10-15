704904ac1998d5f9cfefe59194a8cb4a14d65bfe
irene <18404969346@163.com>
2019-10-15 22:55:27 +0800

    create answer

---
+++ irene.ts
@@ -0,0 +1,31 @@
+// booealn
+
+let a: boolean = true;
+
+// 数字
+
+let n: number = 3;
+
+// 字符串
+
+let s: string = 'irene'
+// 数组
+
+let t: number[] = [2, 3, 4]
+let tt: Array<number> = [24, 5]
+// 元组
+let x: [string, number] = ['irene', 10]
+// 枚举
+enum Color { green, red, yellow }
+let c: Color = Color.green
+// any
+let xs: any = 'irene'
+let xss: any = 2
+// void
+
+// null undefined
+let ns: null = null
+let ss: undefined = undefined
+// never
+// object
+// 类型断言
